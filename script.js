37d0457f1d851c7c5b3b183116a3a9117570f71b
achaja53-creator <achaja53@gmail.com>
2026-07-08 22:38:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,25 @@ function typeLetter(){
     },30);
 
 }
+
+// ==========================
+// Membuat bintang
+// ==========================
+
+const stars = document.getElementById("stars");
+
+for(let i=0;i<120;i++){
+
+    let star=document.createElement("div");
+
+    star.className="star";
+
+    star.style.left=Math.random()*100+"vw";
+
+    star.style.top=Math.random()*100+"vh";
+
+    star.style.animationDuration=(1+Math.random()*3)+"s";
+
+    stars.appendChild(star);
+
+}
